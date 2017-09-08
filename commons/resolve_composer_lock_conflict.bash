3175a4461a4318139fa85d1dae7d79dfa0131422
Ryan Williams <ryrowi@gmail.com>
2017-09-08 10:23:59 -0400

    add resolve_composer_lock_conflict.bash

---
+++ commons/resolve_composer_lock_conflict.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# when two branches have different composer installs, merging can create a conflict that only affects the hash.
+#
+# this script attempts to automate resolving that conflict by:
+#   removing the conflict markers from the composer.lock file,
+#   running the most recent composer require again,
+#   and staging the resulting composer.lock with the updated hash.
+#
+# run this from inside /srv/www/commons/current
+
+set -ex
+
+sed -i '7d;8d;9d;11d' composer.lock
+
+$(git log --grep='composer require' -1 --format=%B)
+
+git add composer.lock
+
+echo 'if all looks good, run git commit now to finish the merge'
