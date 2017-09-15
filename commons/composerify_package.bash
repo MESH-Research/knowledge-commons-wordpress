866bec016288abad5cb4461798a17abaea5d9dad
Ryan Williams <ryrowi@gmail.com>
2017-09-15 13:35:16 -0400

    allow user input override for author, name, version in composerify_package.bash

--- commons/composerify_package.bash
+++ commons/composerify_package.bash
@@ -13,20 +13,33 @@
 # cd path/to/unzipped/theme/
 # ~/code/dev-scripts/commons/composerify_package.bash
 #
+# optional parameters, if empty the script will try to parse on its own
+# $1 author
+# $2 name
+# $3 version
+#
 
 set -ex
 
-if [[ -e 'style.css' ]]
+# first determine package type
+type=plugin
+[[ -e 'style.css' ]] && type=theme
+
+# read user input if provided, otherwise parse according to type
+if [[ -n "$1" ]]
+then
+	author="$1"
+	name="$2"
+	version="$3"
+elif [[ "$type" = 'theme' ]]
 then
 	author=$(grep '^Author:' style.css | awk '{print $2}')
 	name=$(grep '^Theme Name:' style.css | awk '{print $3}' | tr '[:upper:]' '[:lower:]')
 	version=$(grep '^Version:' style.css | awk '{print $2}')
-	type=theme
 else
 	author=$(grep '^Author:' *php | awk '{print $2}')
 	name=$(grep '^Plugin Name:' *php | awk '{print $3}' | tr '[:upper:]' '[:lower:]')
 	version=$(grep '^Version:' *php | awk '{print $2}')
-	type=plugin
 	echo 'plugin support still experimental, check for correct parsing before continuing'
 	exit 1
 fi
