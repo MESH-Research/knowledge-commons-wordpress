e7617c676e2409928bea7449108246e38e18a1d3
Ubuntu <ubuntu@>
2017-10-07 14:29:50 +0000

    automate idpolr sync

--- idpolr/sync_from_idpolr.bash
+++ idpolr/sync_from_idpolr.bash
@@ -6,7 +6,7 @@ stop_prod_services() {
 }
 
 start_prod_services() {
-  ssh $remote_user@$remote_hostname "sudo service slapd starte
+  ssh $remote_user@$remote_hostname "sudo service slapd start"
 }
 
 stop_dev_services() {
@@ -25,26 +25,29 @@ copy_dump() {
   rsync -zhP $remote_user@$remote_hostname:$dump_path/$dump_name $dump_path/$dump_name
 }
 
-# TODO should use variables here rather than hardcode
 edit_dump() {
-  sed -i "/structuralObjectClass:.*/d" $dump_path/$dump_name
-  sed -i "/entryUUID:.*/d" $dump_path/$dump_name
-  sed -i "/creatorsName:.*/d" $dump_path/$dump_name
-  sed -i "/createTimestamp:.*/d" $dump_path/$dump_name
-  sed -i "/entryCSN:.*/d" $dump_path/$dump_name
-  sed -i "/modifiersName:.*/d" $dump_path/$dump_name
-  sed -i "/modifyTimestamp:.*/d" $dump_path/$dump_name
-  sed -i "/pwmGUID:.*/d" $dump_path/$dump_name
-  sed -i "/pwdChangedTime:.*/d" $dump_path/$dump_name
-  sed -i "/pwmLastPwdUpdate:.*/d" $dump_path/$dump_name
-  sed -i "/pwdFailureTime:.*/d" $dump_path/$dump_name
+  #one line per attribute
+  awk 'NR>1 && !sub(/^ /,""){print s; s=""} {s = s $0} END{print s}' $dump_path/$dump_name > $dump_path/$edit_name
+  #remove these attributes
+  sed -i "/structuralObjectClass:.*/d" $dump_path/$edit_name
+  sed -i "/entryUUID:.*/d" $dump_path/$edit_name
+  sed -i "/creatorsName:.*/d" $dump_path/$edit_name
+  sed -i "/createTimestamp:.*/d" $dump_path/$edit_name
+  sed -i "/entryCSN:.*/d" $dump_path/$edit_name
+  sed -i "/modifiersName:.*/d" $dump_path/$edit_name
+  sed -i "/modifyTimestamp:.*/d" $dump_path/$edit_name
+  sed -i "/pwmGUID:.*/d" $dump_path/$edit_name
+  sed -i "/pwdChangedTime:.*/d" $dump_path/$edit_name
+  sed -i "/pwmLastPwdUpdate:.*/d" $dump_path/$edit_name
+  sed -i "/pwdFailureTime:.*/d" $dump_path/$edit_name
+  sed -i "/pwmEventLog::.*/d" $dump_path/$edit_name
 }
 
 import_slap() {
   cd /var/lib/ldap
   sudo rm __db.*
   sudo rm *.bdb
-  sudo slapadd -v -c -n 1 -l $dump_path/$dump_name
+  sudo slapadd -v -c -n 1 -l $dump_path/$edit_name
   ls -l
   sudo chown openldap:openldap log.0000*
   sudo chown openldap:openldap __db.*
@@ -57,6 +60,7 @@ import_slap() {
 clean_up() {
   ssh $remote_user@$remote_hostname "rm -v $dump_path/$dump_name"
   rm -v $dump_path/$dump_name
+  rm -v $dump_path/$edit_name
 }
 
 remote_hostname=hc-idp.hcommons.org
@@ -64,6 +68,7 @@ remote_user=ubuntu
 
 # same name & path on prod & dev
 dump_name=idpolr_slapcat_latest.out
+edit_name=idpolr_slapcat_latest.edit
 dump_path=/tmp
 
 #echo "disabled for now. read the source"
@@ -74,10 +79,9 @@ dump_path=/tmp
 
 #copy_dump
 #edit_dump
-#
+
 #stop_dev_services
 #import_slap
-
 #start_dev_services 
 
 #clean_up
