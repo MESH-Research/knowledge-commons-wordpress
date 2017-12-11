2176bc8a2a000c1acb28b056ac02f3b3915915f1
Ryan Williams <rwilliams@mla.org>
2017-12-11 13:57:09 -0500

    support configurable branch in composer_require.bash

--- commons/composer_require.bash
+++ commons/composer_require.bash
@@ -3,13 +3,20 @@ set -ex
 
 # usage:
 # after updating a package already required by composer,
-# run this script & pass the package name to make composer require the latest version
+# run this script & pass the package name, & optionally branch (default master),
+# to make composer require the latest version
 # e.g.
 #
 # composer_require.bash mlaa/hc-custom
+# composer_require.bash mlaa/hc-custom develop
 
 package="$1" # e.g. mlaa/hc-custom
-branch="master" # TODO configurable
+if [[ -n "$2" ]]
+then
+	branch="$2"
+else
+	branch="master"
+fi
 ref="$(git ls-remote git://github.com/$package.git | grep $branch)"
 
 cmd="composer require $package:dev-$branch#${ref:0:7}"
