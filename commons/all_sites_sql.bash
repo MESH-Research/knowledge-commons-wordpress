8d7d9ed404a1d685db8e5c9c90c2e6943b3b7286
Ryan Williams <rwilliams@mla.org>
2018-01-22 13:58:43 -0500

    WIP updates for all_sites_sql.bash

--- commons/all_sites_sql.bash
+++ commons/all_sites_sql.bash
@@ -4,21 +4,20 @@ set -ex
 echo 'not finished yet, read the source'
 exit
 
-tables="$(mysql -Be "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_NAME LIKE 'wp_%options'")"
-results=()
+blog_ids="$(mysql -BNe 'SELECT blog_id FROM hcommons.wp_blogs')"
 
-for table in $tables
+for blog_id in $blog_ids
 do
-  [[ "$table" = "TABLE_NAME" ]] && continue # TODO do better
+  options_table=wp_${blog_id}_options
 
-  #results+=("$(mysql hcommons -Be "SELECT option_value FROM $table WHERE option_name = 'current_theme'")")
-  mysql hcommons -Be "SELECT option_value FROM $table WHERE option_name = 'current_theme'"
-done
+  if [[ -n "$(mysql hcommons -Be "show tables like '$options_table'")" ]]
+  then
+    mysql hcommons -BNe "SELECT domain FROM wp_blogs WHERE blog_id = $blog_id"
+    mysql hcommons -BNe "SELECT option_value FROM $options_table WHERE option_name = 'current_theme'"
+  else
+    echo "no such table '$options_table'"
+  fi
 
-
-for result in $results
-do
-  echo $result
 done
 
 
