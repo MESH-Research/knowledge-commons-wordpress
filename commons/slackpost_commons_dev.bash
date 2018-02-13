2fb3afabd74187a047dfc05655b8b548e5b698ea
Ryan Williams <rwilliams@mla.org>
2018-02-13 16:49:14 -0500

    call slackpost with bash explicitly

--- commons/slackpost_commons_dev.bash
+++ commons/slackpost_commons_dev.bash
@@ -4,4 +4,4 @@ set -ex
 
 # wrapper for slackpost.bash that posts $1 to #commons-dev
 
-~/dev-scripts/commons/slackpost.bash 'https://hooks.slack.com/services/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' '#commons-dev' 'platybot' "$1"
+bash ~/dev-scripts/commons/slackpost.bash 'https://hooks.slack.com/services/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' '#commons-dev' 'platybot' "$1"
