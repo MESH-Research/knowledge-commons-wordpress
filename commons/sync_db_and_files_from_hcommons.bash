0863e43a649189851a25afa9dccca4309f949dd0
Ubuntu <ubuntu@ip-10-98-11-147.ec2.internal>
2019-04-08 19:40:25 +0000

    Cleanup sync db and files script.

--- commons/sync_db_and_files_from_hcommons.bash
+++ commons/sync_db_and_files_from_hcommons.bash
@@ -1,33 +1,48 @@
 #!/bin/bash
 set -ex
 
-remote_hostname=hcommons.org
-remote_user=ubuntu
+# echo env variable values
+# $1 name of .env variable
+# $2 server from which to fetch value
+_get_remote_env_var() {
+  local line=$(ssh $remote_user@$2 "grep '^$1' /srv/www/commons/current/.env")
+  echo $line | awk -F '=' '{print $2}' | tr -d '"' | tr -d "'"
+}
 
-dev_domain=hcommons-dev.org
-prod_domain=$remote_hostname
+# echo env variable values
+# $1 name of .env variable
+# $2 server from which to fetch value
+_get_local_env_var() {
+  local line=$(grep ^$1 /srv/www/commons/current/.env)
+  echo $line | awk -F '=' '{print $2}' | tr -d '"' | tr -d "'"
+}
 
-# TODO find a more secure way to store credentials?
-prod_db_name=hcommons
-prod_db_user=hcdb
-prod_db_host=hcommons-prod-wordpress.cyongorao4kh.us-east-1.rds.amazonaws.com
-prod_db_pass=hAXzDgVuWsrWSdIOZqvDse
-#dev_db_name=hcommons_dev
-dev_db_name=hcommons
-dev_db_user=hcommons_dev
-dev_db_host=hcommons-dev-wordpress.cyongorao4kh.us-east-1.rds.amazonaws.com
-dev_db_pass=sANQ8NF5DkgSsIT7SskbxO
+setup_data() {
+  remote_hostname=hcommons.org
+  remote_user=ubuntu
+
+  dev_domain="$(hostname)"
+  prod_domain=$remote_hostname
+
+  prod_db_name=$(_get_remote_env_var DB_NAME $remote_hostname)
+  prod_db_host=$(_get_remote_env_var DB_HOST $remote_hostname)
+  prod_db_pass=$(_get_remote_env_var DB_PASS $remote_hostname)
+  prod_db_user=$(_get_remote_env_var DB_USER $remote_hostname)
+
+  dev_db_name=$(_get_local_env_var DB_NAME)
+  dev_db_host=$(_get_local_env_var DB_HOST)
+  dev_db_pass=$(_get_local_env_var DB_PASS)
+  dev_db_user=$(_get_local_env_var DB_USER)
 
 #dump_name=${prod_db_name}_$(date +%Y%m%dT%H%M%S).sql
-dump_name=${prod_db_name}_latest.sql
-dump_path=/tmp
+  dump_name=${prod_db_name}_latest.sql
+  dump_path=/tmp
+  project_path=/srv/www/commons/current
+  shared_path=/srv/www/commons/shared
+echo $dev_db_host
+}
 
-project_path=/srv/www/commons/current
-
-shared_path=/srv/www/commons/shared
-
-# TODO we should do this from a stored backup rather than live sync.
-sync_db() {
+export_db() {
   # --lock-tables=false may result in inconsistent dump, but prevents bringing production down while dumping
   # changed dump path due to low available space on root drive. this one is EFS
   ssh $remote_user@$remote_hostname "mysqldump\
@@ -38,8 +53,12 @@ sync_db() {
     --lock-tables=false\
     --quick\
     > /mnt/efs-logs/$dump_name"
+}
 
-  rsync -zhP --remove-source-files $remote_user@$remote_hostname:/mnt/efs-logs/$dump_name $dump_path/$dump_name
+sync_db() {
+
+  #rsync -zhP --remove-source-files $remote_user@$remote_hostname:/mnt/efs-logs/$dump_name $dump_path/$dump_name
+  rsync -zhP $remote_user@$remote_hostname:/mnt/efs-logs/$dump_name $dump_path/$dump_name
 
   # DO NOT USE. breaks serialized data. use search-replace instead (below)
   #sed -i "s/$prod_domain/$dev_domain/g" $dump_path/$dump_name
@@ -47,13 +66,15 @@ sync_db() {
   mysql --max_allowed_packet=100M -u$dev_db_user -p$dev_db_pass -h$dev_db_host $dev_db_name < "$dump_path/$dump_name"
 
   #ssh $remote_user@$remote_hostname "rm -v $dump_path/$dump_name"
-  rm -v $dump_path/$dump_name
+  #rm -v $dump_path/$dump_name
 
   sudo -u www-data wp search-replace\
     --url="$prod_domain"\
     --all-tables\
     --path=/srv/www/commons/current/web/wp\
     "$prod_domain" "$dev_domain"
+
+  wp --url=hcommons.org --path=/srv/www/commons/current/web/wp cache flush
 
   # fix email addresses
   sudo -u www-data wp search-replace\
@@ -68,6 +89,7 @@ sync_db() {
     --all-tables\
     --path=/srv/www/commons/current/web/wp\
     "style.mla.org" "style.mla-dev.org"
+
   sudo -u www-data wp search-replace\
     --url="$dev_domain"\
     --all-tables\
@@ -86,12 +108,12 @@ sync_db() {
   #done
 
   cd $project_path
-  ~/all_networks_wp.bash cache flush
+  ~/dev-scripts/commons/all_networks_wp.bash cache flush
 
   # disable most emails (until a wp action changes them back)
-  mysql -u$dev_db_user -p$dev_db_pass -h$dev_db_host $dev_db_name -e "update wp_users set user_email=replace(user_email,'@','@sign') where ID not in (1,1937,4381);"
+  mysql -u$dev_db_user -p$dev_db_pass -h$dev_db_host $dev_db_name -e "update wp_users set user_email=replace(user_email,'@','@sign') where ID not in (1,4381);"
   # fix whitelisted emails
-  bash ~/dev-scripts/commons/restore_user_emails.bash adonlon nalonso eknappe rwms
+  bash ~/dev-scripts/commons/restore_user_emails.bash adonlon nalonso eknappe jbetancourt
 
   # handle sparkpost/bp-rbe settings
   bash /home/ubuntu/dev-scripts/commons/set_sp_settings_dev.bash
@@ -106,7 +128,7 @@ sync_files() {
 # depends on all_networks_wp.bash
 activate_plugins() {
   cd $project_path
-  ~/all_networks_wp.bash plugin activate --network\
+  ~/dev-scripts/commons/all_networks_wp.bash plugin activate --network\
     debug-bar\
     debug-bar-actions-and-filters-addon\
     wordpress-debug-bar-template-trace\
@@ -116,10 +138,11 @@ activate_plugins() {
 }
 
 OPTIND=1
-while getopts "h?dfp" opt; do
+while getopts "h?defp" opt; do
   case "$opt" in
     h|\?) echo "i should probably write help output sometime. until then, read the source"; exit 0;;
     d) d=1;;
+    e) e=1;;
     f) f=1;;
     p) p=1;;
   esac
@@ -128,17 +151,21 @@ shift $((OPTIND-1))
 if [ "$1" = -- ]; then shift; fi
 
 # if no options were passed, do everything
-if [[ -z "$r$d$f$p" ]]
+if [[ -z "$r$d$e$f$p" ]]
 then
-  bash ~/dev-scripts/commons/slackpost_commons_dev.bash 'hcommons-dev.org sync starting'
+  bash ~/dev-scripts/commons/slackpost_commons_dev.bash "$(hostname) sync starting"
+  setup_data
+  export_db
   sync_db
   sync_files
   #activate_plugins
-  bash ~/dev-scripts/commons/slackpost_commons_dev.bash 'hcommons-dev.org sync finished'
+  bash ~/dev-scripts/commons/slackpost_commons_dev.bash "$(hostname) sync finished"
   exit
 fi
 
 # otherwise just do what was asked
+setup_data
+if [[ -n "$e" ]]; then export_db; fi
 if [[ -n "$d" ]]; then sync_db; fi
 if [[ -n "$f" ]]; then sync_files; fi
 if [[ -n "$p" ]]; then activate_plugins; fi
