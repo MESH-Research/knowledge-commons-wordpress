5983d6532d293311caae4d88ceb814939e0971f7
Mike Thicke <thickemi@msu.edu>
2021-11-03 16:50:37 +0000

    add repo-status script

---
+++ commons/repo-status.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+'''
+Survey the Humanities Commons git repositories active on the WordPress server
+and report their status.
+'''
+
+import subprocess
+import re
+
+REPOS = {
+	'aupresses-jobs'                      : '/srv/www/commons/current/web/app/themes/aupresses-jobs',
+	'bbPress-Export-and-Import'           : '/srv/www/commons/current/web/app/plugins/bbPress-Export-and-Import',
+	'boss-child'                          : '/srv/www/commons/current/web/app/themes/boss-child',
+	'bp-attachment-xprofile-field-type'   : '/srv/www/commons/current/web/app/plugins/bp-attachment-xprofile-field-type',
+	'bp-block-member'                     : '/srv/www/commons/current/web/app/plugins/bp-block-member',
+	'bp-event-organiser'                  : '/srv/www/commons/current/web/app/plugins/bp-event-organiser',
+	'bp-group-documents'                  : '/srv/www/commons/current/web/app/plugins/bp-group-documents',
+	'bp-mpo-activity-filter'              : '/srv/www/commons/current/web/app/plugins/bp-mpo-activity-filter',
+	'bp-reply-by-email'                   : '/srv/www/commons/current/web/app/plugins/bp-reply-by-email',
+	'buddypress-docs-minor-edit'          : '/srv/www/commons/current/web/app/plugins/buddypress-docs-minor-edit',
+	'buddypress-group-email-subscription' : '/srv/www/commons/current/web/app/plugins/buddypress-group-email-subscription',
+	'buddypress-messages-spam-blocker'    : '/srv/www/commons/current/web/app/plugins/buddypress-messages-spam-blocker',
+	'bwp-google-xml-sitemaps'             : '/srv/www/commons/current/web/app/plugins/bwp-google-xml-sitemaps',
+	'cbox-mla-blog'                       : '/srv/www/commons/current/web/app/themes/cbox-mla-blog',
+	'commentpress-mla'                    : '/srv/www/commons/current/web/app/themes/commentpress-mla',
+	'commentpress-mla-digitalpedagogy'    : '/srv/www/commons/current/web/app/themes/commentpress-mla-digitalpedagogy',
+	'commentpress-mla-groups'             : '/srv/www/commons/current/web/app/themes/commentpress-mla-groups',
+	'commons'                             : '/srv/www/commons/current',
+	'elasticpress-buddypress'             : '/srv/www/commons/current/web/app/plugins/elasticpress-buddypress',
+	'faculty-child'                       : '/srv/www/commons/current/web/app/themes/faculty-child',
+	'hc-auth'                             : '/srv/www/commons/current/web/app/plugins/hc-auth',
+	'hc-custom'                           : '/srv/www/commons/current/web/app/plugins/hc-custom',
+	'hc-member-profiles'                  : '/srv/www/commons/current/web/app/plugins/hc-member-profiles',
+	'hc-notifications'                    : '/srv/www/commons/current/web/app/plugins/hc-notifications',
+	'hc-organizations'                    : '/srv/www/commons/current/web/app/plugins/hc-organizations',
+	'hc-protect-uploads'                  : '/srv/www/commons/current/web/app/plugins/hc-protect-uploads',
+	'hc-provisional-content'              : '/srv/www/commons/current/web/app/plugins/hc-provisional-content',
+	'hc-styles'                           : '/srv/www/commons/current/web/app/plugins/hc-styles',
+	'hc-suggestions'                      : '/srv/www/commons/current/web/app/plugins/hc-suggestions',
+	'humanities-commons'                  : '/srv/www/commons/current/web/app/plugins/humanities-commons',
+	'humcore'                             : '/srv/www/commons/current/web/app/plugins/humcore',
+	'mla-academic-interests'              : '/srv/www/commons/current/web/app/plugins/mla-academic-interests',
+	'mla-admin-bar'                       : '/srv/www/commons/current/web/app/plugins/mla-admin-bar',
+	'mla-allowed-tags'                    : '/srv/www/commons/current/web/app/plugins/mla-allowed-tags',
+	'mla-login-bar'                       : '/srv/www/commons/current/web/app/plugins/mla-login-bar',
+	'password-protected'                  : '/srv/www/commons/current/web/app/plugins/password-protected',
+	'plugin-monitor'                      : '/srv/www/commons/current/web/app/plugins/plugin-monitor',
+	'shibboleth'                          : '/srv/www/commons/current/web/app/plugins/shibboleth',
+	'sparkpost-bp-mailer'                 : '/srv/www/commons/current/web/app/plugins/sparkpost-bp-mailer',
+	'wordpress-sparkpost'                 : '/srv/www/commons/current/web/app/plugins/wordpress-sparkpost',
+	'wp-saml-auth'                        : '/srv/www/commons/current/web/app/mu-plugins/wp-saml-auth',
+	'zotpress'                            : '/srv/www/commons/current/web/app/plugins/zotpress'
+}
+
+def get_repo_info( dir ) :
+	'''
+	Get updated status information for a particular repository.
+
+	Args:
+		dir: Directory the repository is located in
+	
+	Returns:
+		A dict with repository information:
+
+		{
+			'branch'      : The current local branch or None if no repository
+			'remote'      : The remote branch being tracked
+			'status'      : 'current' | 'ahead' | 'behind' | 'diverged'
+			'uncommitted' : bool - are there unstaged changes?
+			'untracked'   : bool - are there untracked files?
+		}
+	'''
+	result_dict = {
+		'branch'      : 'None',
+		'remote'      : 'None',
+		'status'      : 'None',
+		'uncommitted' : False,
+		'untracked'   : False
+	}
+	git_status = subprocess.run(
+		['git', 'status'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.DEVNULL,
+		cwd=dir,
+		universal_newlines=True
+	).stdout
+	branch_regex = re.compile( 'On branch (.*)')
+	match = branch_regex.search( git_status )
+	if match is None :
+		result_dict['branch'] = 'HEAD detached'
+		return result_dict
+	result_dict['branch'] = match.group( 1 )
+	remote_regex = re.compile( 'Your branch .*? \'(.*?)\'' )
+	match = remote_regex.search( git_status )
+	if match is None :
+		result_dict['remote'] = 'None'
+	else :
+		result_dict['remote'] = match.group( 1 )
+	
+	if git_status.find( 'Your branch is up to date' ) > -1 :
+		result_dict['status'] = 'current'
+	elif git_status.find( 'Your branch is ahead of' ) > -1 : 
+		result_dict['status'] = 'ahead'
+	elif git_status.find( 'Your branch is behind' ) > -1 :
+		result_dict['status'] = 'behind'
+	else :
+		result_dict['status'] = 'diverged'
+	
+	if git_status.find( 'nothing to commit, working tree clean' ) > -1 :
+		result_dict['uncommitted'] = False
+		result_dict['untracked'] = False
+	elif git_status.find( 'Changes to be committed' ) > -1 or git_status.find( 'Changes not staged for commit') > -1 :
+		result_dict['uncommitted'] = True
+	elif git_status.find( 'Untracked files' ) > 1 :
+		result_dict['untracked'] = True
+
+	return result_dict
+
+def make_repo_table( repo_dict ) :
+	'''
+	Construct a table of repo statuses.
+
+	Args:
+		repo_dict: Dictionay of repo_name : repo_directory
+	
+	Returns:
+		Array of dicts containing repo statuses
+	'''
+	repo_table = []
+	for name, directory in repo_dict.items() :
+		repo_info = get_repo_info( directory )
+		repo_info['name'] = name
+		repo_table.append( repo_info )
+	return repo_table
+
+def print_repo_table( repo_table ) :
+	'''
+	Outputs a table of repo info to the console.
+
+	Args:
+		repo_table: Array of dicts containing repository info
+	'''
+	print ( '{:37} {:13} {:15} {:8} {:5} {:5}'.format( 'Name', 'Branch', 'Remote', 'Status', 'UC', 'UT' ) )
+	print ( '{:37} {:13} {:15} {:8} {:5} {:5}'.format( '----', '------', '------', '------', '--', '--' ) )
+	for row in repo_table :
+		print ( '{:37.37} {:13.13} {:15.15} {:8.8} {:5.5} {:5.5}'.format( 
+			row['name'],
+			row['branch'],
+			row['remote'],
+			row['status'],
+			str( row['uncommitted']),
+			str( row['untracked'] )
+		) )
+
+if __name__ == '__main__' :
+	repo_table = make_repo_table( REPOS )
+	print_repo_table( repo_table )
