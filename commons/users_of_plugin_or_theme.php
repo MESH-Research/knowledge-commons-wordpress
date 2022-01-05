e5b193fc18a50c56f626b0fb3830dfe7524bb84e
Ubuntu <ubuntu@hcommons.org>
2022-01-05 15:40:29 +0000

    hc-admin-docs-support-54-initial-script-to-generate-users-of-plugin-script

---
+++ commons/users_of_plugin_or_theme.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Generates a CSV of sites using a particular plugin or theme in order to email
+ * them.
+ * 
+ * Syntax: wp eval-file users_of_plugin_or_theme.php <plugin|theme> <slug> > <outputfile.csv>
+ * Example: wp eval-file user_of_plugin_or_theme.php plugin google-document-embedder > ~/outputfile.csv
+ */
+
+
