d7c54804b67686b390fd46d1486f39083f3ed0ce
Mike Thicke <thickemi@msu.edu>
2022-08-30 14:53:02 +0000

    hc-admin-docs-support-310-rebuild-user_blogs

---
+++ commons/sync_bp_user_blogs.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Syncs user site roles to the BuddyPress wp_bp_user_blogs table.
+ *
+ * The BuddyPress table wp_bp_user_blogs keeps track of site membership for
+ * BuddyPress components, such as those on user profile pages. It is possible
+ * for site roles as reflected in wp_usermeta to get out of sync with those in
+ * wp_bp_user_blogs, in which case those blogs, or posts within those blogs,
+ * won't show up on their profiles.
+ *
+ * This script will add users to sites in the wp_bp_user_blogs table if they
+ * have author, editor, or administrator roles on those sites, which corrects
+ * the above issue.
+ *
+ * @see https://github.com/MESH-Research/hc-admin-docs-support/issues/310
+ *
+ * Note: bp_blogs_record_existing_blogs() rebuilds everything from scratch, so
+ * the script takes some time and there is a risk of unintended consequences.
+ * Should always run on dev first and test before running on prod.
+ *
+ * Usage: wp eval-file sync_bp_user_blogs.php
+ */
+
+/**
+ * This function calls bp_blogs_record_existing_blogs() to rebuild the wp_bp_user_blogs table.
+ * 
+ */
+function buddypress_repair() {
+	foreach ( get_networks() as $network ) {
+		echo "Recording blogs for network id {$network->id}...\n";
+		$blogs = get_sites(
+			[
+				'fields' => 'ids',
+				'number' => 99999,
+				'network_id' => $network->id
+			]
+		);
+
+		echo "Recording membership for " . count( $blogs ) . " sites.\n";
+		
+		$success = bp_blogs_record_existing_blogs(
+			[
+				'limit'    => 99999,
+				'site_id'  => $network->id,
+				'blog_ids' => $blogs,
+			]
+		);
+
+		if ( $success ) {
+			echo "...success\n";
+		} else {
+			echo "...failed\n";
+		}
+	}
+}
+
+buddypress_repair();
