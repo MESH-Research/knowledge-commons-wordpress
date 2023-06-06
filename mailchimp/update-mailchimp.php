a7bed337b3ee029a7ede6e306ed60927fd08bcca
Mike Thicke <thickemi@msu.edu>
2023-06-06 13:46:08 +0000

    commons-617-finalize-mailchimp-sync

--- mailchimp/update-mailchimp.php
+++ mailchimp/update-mailchimp.php
@@ -2,7 +2,13 @@
 /**
  * Updates MailChimp email list with current users.
  *
- * Usage: wp eval-file update-mailchimp.php [weeks] [export]
+ * Usage: 	wp eval-file update-mailchimp.php weeks=<weeks> [csv=filename.csv] [dry-run=true] [mail=email@address.com,another@address.com]
+ * 
+ * 			Parameters must be passed as key=value (no spaces).
+ * 				weeks: Number of weeks to look back for recent users.
+ * 				csv: Optional. Filename to save CSV of recent users.
+ * 				dry-run: Optional. If true, no changes will be made to MailChimp.
+ * 				mail: Optional. If set, an update email will be sent to the specified addresses.
  */
 
 require_once( __DIR__ . '/vendor/autoload.php');
@@ -13,10 +19,10 @@ const API_KEY = '7ec2ee0ed5617b202d903aee9b68a535-us9';
 const DC = 'us9';
 const LIST_ID = 'ab124b16b0'; //commons-active
 
-const RECENT_WEEKS = 50;
-const MAX_USERS = 0;
+const HC_ROOT_BLOG_ID = 1000360;
 
-const DRY_RUN = false;
+// Facilitate debugging MailChimp API responses.
+ini_set( 'log_errors_max_len', 0 );
 
 /**
  * Main function.
@@ -24,27 +30,82 @@ const DRY_RUN = false;
 function main( $args ) {
 	log_entry( 'Beginning MailChimp update.' );
 
-	if ( $args ) {
-		$weeks = $args[0];
-	} else {
-		$weeks = RECENT_WEEKS;
+	try {
+		$args = parse_args( $args );
+		validate_args( $args );
+	} catch ( Exception $e ) {
+		log_entry( $e->getMessage() );
+		exit;
 	}
-	
-	log_entry( "Recent weeks: $weeks Max users: " . MAX_USERS );
 
-	$recent_users = get_recent_users( $weeks, MAX_USERS );
+	$recent_users = get_recent_users( $args['weeks'] );
 
 	log_entry( 'Found ' . count( $recent_users ) . ' recent users.' );
-	
-	if ( ! DRY_RUN ) {
-		update_mailchimp( $recent_users );
+
+	$dry_run = array_key_exists( 'dry-run', $args ) && $args['dry-run'];
+	if ( ! $dry_run ) {
+		$updated_users = update_mailchimp( $recent_users );
 	}
 	
-	if ( count( $args ) > 1 && 'export' === $args[1] ) {
-		export_users_as_csv( $recent_users );
+	if ( $args['csv'] ) {
+		export_users_as_csv( $recent_users, $args['csv'] );
+	}
+
+	if ( $args['mail'] ) {
+		if ( $args['csv'] ) {
+			send_update_email( $args['mail'], $updated_users, $args['csv'] );
+		} else {
+			send_update_email( $args['mail'], $updated_users );
+		}
 	}
 
 	log_entry( 'Finished MailChimp update.' );
+}
+
+/**
+ * Parse arguments into associative array.
+ *
+ * Arguments should be passed to script as key=value (no spaces).
+ *
+ * @param Array $args Array of arguments as passed to script by WP CLI eval-file.
+ */
+function parse_args( $args ) {
+	$parsed_args = [];
+	foreach ( $args as $arg ) {
+		$split_arg = explode( '=', $arg );
+		if ( count( $split_arg ) !== 2 ) {
+			throw new Exception( "Misformatted argument: '$arg'" );
+		}
+		if ( is_numeric( $split_arg[1] ) ) {
+			$split_arg[1] = intval( $split_arg[1] );
+		} elseif ( 'true' === $split_arg[1] ) {
+			$split_arg[1] = true;
+		} elseif ( 'false' === $split_arg[1] ) {
+			$split_arg[1] = false;
+		} elseif ( strpos( $split_arg[1], ',' ) !== false ) {
+			$split_arg[1] = explode( ',', $split_arg[1] );
+		}
+		$parsed_args[ $split_arg[0] ] = $split_arg[1];
+	}
+	return $parsed_args;
+}
+
+/**
+ * Validate arguments.
+ */
+function validate_args( $args ) {
+	if ( ! array_key_exists( 'weeks', $args ) ) {
+		throw new Exception( 'Missing required argument: weeks' );
+	}
+	if ( ! is_numeric( $args['weeks'] ) ) {
+		throw new Exception( 'Invalid argument: weeks must be numeric' );
+	}
+	if ( array_key_exists( 'csv', $args ) && ! is_string( $args['csv'] ) ) {
+		throw new Exception( 'Invalid argument: csv must be string' );
+	}
+	if ( array_key_exists( 'dry-run', $args ) && ! is_bool( $args['dry-run'] ) ) {
+		throw new Exception( 'Invalid argument: dry-run must be boolean' );
+	}
 }
 
 /**
@@ -53,13 +114,13 @@ function main( $args ) {
  * @param int $weeks Number of weeks to look back.
  * @param int $max_users Maximum number of users to return or 0 for no limit.
  */
-function get_recent_users( $weeks, $max_users = 0 ) {
+function get_recent_users( $weeks ) {
 	global $wpdb;
 
 	$cutoff_time = time() - ( $weeks * WEEK_IN_SECONDS );
 	
 	// Only include HASTAC users who have been active since 9/22/2022 (HASTAC import date)
-	$hastac_cutoff_time = mktime( 0, 0, 0, 9, 22, 2022 );
+	$hastac_cutoff_time = mktime( 0, 0, 0, 9, 23, 2022 );
 
 	if ( $cutoff_time > $hastac_cutoff_time ) {
 		$hastac_cutoff_time = $cutoff_time;
@@ -67,62 +128,151 @@ function get_recent_users( $weeks, $max_users = 0 ) {
 
 	$bp_activity_table = buddypress()->members->table_name_last_activity;
 
-	$sql = "SELECT u.ID, u.user_email, u.display_name, s.meta_value AS session_tokens, a.date_recorded AS last_activity 
+	$sql = "SELECT 	u.ID, 
+					u.user_email, 
+					u.display_name, 
+					u.user_login,
+					FROM_UNIXTIME( 
+						CAST( 
+							REGEXP_SUBSTR(
+								REGEXP_SUBSTR( s.meta_value, '\"login\";i:[0-9]+;}}' ),
+								'[0-9]+'
+							) 
+							AS UNSIGNED 
+						)
+					) AS last_session,
+					a.date_recorded AS last_activity, 
+					n.meta_value AS newsletter_optin,
+					m.name AS member_type
 			FROM $wpdb->users AS u
 			LEFT JOIN $wpdb->usermeta AS s ON s.user_id = u.ID AND s.meta_key = 'session_tokens'
-			LEFT JOIN $wpdb->usermeta AS n ON n.user_id = u.ID AND n.meta_key = 'newsletter_optin'
 			LEFT JOIN $bp_activity_table AS a ON a.user_id = u.ID AND a.type = 'last_activity'
 			LEFT JOIN wp_1000360_term_relationships as r ON u.ID = r.object_id
-			LEFT JOIN wp_1000360_term_taxonomy as t ON r.term_taxonomy_id = t.term_taxonomy_id
+			LEFT JOIN wp_1000360_term_taxonomy as t ON r.term_taxonomy_id = t.term_taxonomy_id AND t.taxonomy = 'bp_member_type'
 			LEFT JOIN wp_1000360_terms as m ON t.term_id = m.term_id
+			LEFT JOIN $wpdb->usermeta AS n ON n.user_id = u.ID AND n.meta_key = 'newsletter_optin'
 			WHERE (
-				m.name = 'hastac' AND (
-					UNIX_TIMESTAMP( STR_TO_DATE( a.date_recorded, '%Y-%m-%d %H:%i:%s' ) ) > $hastac_cutoff_time
-					OR CAST( 
-						REGEXP_SUBSTR(
-							REGEXP_SUBSTR( s.meta_value, '\"login\";i:[0-9]+;}}' ),
-							'[0-9]+'
-						)
-					AS UNSIGNED ) > $hastac_cutoff_time
-				)
-				OR m.name <> 'hastac' AND (
-					UNIX_TIMESTAMP( STR_TO_DATE( a.date_recorded, '%Y-%m-%d %H:%i:%s' ) ) > $cutoff_time
-					OR CAST( 
-						REGEXP_SUBSTR(
-							REGEXP_SUBSTR( s.meta_value, '\"login\";i:[0-9]+;}}' ),
-							'[0-9]+'
-						)
-					AS UNSIGNED ) > $cutoff_time
-				)
+				UNIX_TIMESTAMP( STR_TO_DATE( a.date_recorded, '%Y-%m-%d %H:%i:%s' ) ) > $cutoff_time
+				OR CAST( 
+					REGEXP_SUBSTR(
+						REGEXP_SUBSTR( s.meta_value, '\"login\";i:[0-9]+;}}' ),
+						'[0-9]+'
+					)
+				AS UNSIGNED ) > $cutoff_time	
 			)
-			AND n.meta_value <> 'no'
+			AND ( n.meta_value IS NULL OR n.meta_value = 'yes' )
 			AND u.deleted = 0
-			AND u.spam = 0";
+			AND u.spam = 0
+			AND u.user_email IS NOT NULL
+			AND u.user_email <> ''";
 	
-	if ( $max_users ) {
-		$sql .= " LIMIT $max_users";
-	}
-
-	// Get all users who have logged in or have activity within $weeks weeks and have not opted-out of the newsletter.
 	$users = $wpdb->get_results( $sql );
 
-	return $users;
+	// The above query returns duplicate users if they have multiple member types or multiple last_activities.
+	// Combine these into a single user object.
+	$unique_users = [];
+	foreach ( $users as $user ) {
+		if ( array_key_exists( $user->ID, $unique_users ) ) {
+			if ( $user->last_login > $unique_users[ $user->ID ]->last_login ) {
+				$unique_users[ $user->ID ]->last_login = $user->last_login;
+			}
+			if ( $user->last_activity > $unique_users[ $user->ID ]->last_activity ) {
+				$unique_users[ $user->ID ]->last_activity = $user->last_activity;
+			}
+			if ( 
+					! in_array( $user->member_type, $unique_users[ $user->ID ]->member_type ) 
+					&& $user->member_type
+			) {
+				$unique_users[ $user->ID ]->member_type[] = $user->member_type;
+			}
+		} else {
+			if ( $user->member_type ) {
+				$user->member_type = [ $user->member_type ];
+			} else {
+				$user->member_type = [];
+			}
+			$unique_users[ $user->ID ] = $user;
+		}
+	}
+
+	// I can't figure out how to do this in the SQL query, so HASTAC users who
+	// have not been active since 9/22/2022 are filtered out here.
+	$hastac_filtered_users = [];
+	foreach ( $unique_users as $user ) {
+		if ( 
+			is_array( $user->member_type ) && 
+			in_array( 'hastac', $user->member_type ) &&
+			in_array( 'hc', $user->member_type ) &&
+			count( $user->member_type ) === 2 &&
+			strtotime( $user->last_activity ) < $hastac_cutoff_time &&
+			strtotime( $user->last_login ) < $hastac_cutoff_time
+		) {
+			continue;
+		}
+		$hastac_filtered_users[] = $user;
+	}
+
+	return $hastac_filtered_users;
 }
 
 /**
- * Export users to CSV.
+ * csv users to CSV.
  * 
  * @param array $users Array of users.
  */
-function export_users_as_csv( $users ) {
+function export_users_as_csv( $users, $csv_file = 'recent-users.csv' ) {
 	log_entry( 'Exporting users to CSV.' );
 
-	$csv = fopen( 'recent-users.csv', 'w' );
-	fputcsv( $csv, [ 'ID', 'Email', 'Session Tokens', 'Last Activity' ] );
+	$csv = fopen( $csv_file, 'w' );
+
+	fputcsv( $csv, array_keys( get_object_vars( reset( $users ) ) ) );
+
 	foreach ( $users as $user ) {
-		fputcsv( $csv, [ $user->ID, $user->user_email, $user->session_tokens, $user->last_activity ] );
+		if ( is_array( $user->member_type ) ) {
+			$user->member_type = implode( '; ', $user->member_type );
+		}
+		fputcsv( $csv, get_object_vars( $user ) );
 	}
+	
 	fclose( $csv );
+}
+
+function send_update_email( $to, $updated_users, $csv_file = null ) {
+	if ( ! is_array( $to ) ) {
+		$to = [ $to ];
+	}
+	
+	log_entry( "Sending CSV by email. To: " . implode( ', ', $to ) );
+
+	$email_body = "MailChimp update complete.\n\n";
+
+	$email_body .= "Added users (". count($updated_users['added']) . "):\n";
+
+	foreach ( $updated_users['added'] as $user ) {
+		$email_body .= "{$user->user_login} : {$user->user_email} \n";
+	}
+
+	$email_body .= "\nRemoved users (" . count( $updated_users['removed'] ) . "):\n";
+
+	foreach ( $updated_users['removed'] as $user ) {
+		$email_body .= "{$user->user_login} : {$user->user_email} \n";
+	}
+
+	if ( $csv_file ) {
+		$attachments = [ $csv_file ];
+	} else {
+		$attachments = [];
+	}
+
+	$result = wp_mail(
+		$to,
+		'MailChimp update complete',
+		$email_body,
+		[],
+		$attachments
+	);
+
+	log_entry( 'Email sent: ' . ( $result ? 'true' : 'false' ) );
 }
 
 /**
@@ -134,13 +284,17 @@ function update_mailchimp( $recent_users, $list_id = LIST_ID ) {
 
 	$current_members = get_current_list_members( $mc, $list_id );
 
-	// Add new users to list.
+	$added_users = [];
+
 	foreach ( $recent_users as $user ) {
 		if ( 
 			! isset( $current_members[ $user->user_email ] ) 
 			|| 'subscribed' !== $current_members[ $user->user_email ]->status
 		) {
 			[ $first_name, $last_name ] = get_first_and_last_name( $user->display_name );
+			if ( ! $user->member_type ) {
+				$user->member_type = [ 'hc' ];
+			}
 			try {
 				$response = $mc->lists->setListMember( 
 					$list_id,
@@ -152,6 +306,7 @@ function update_mailchimp( $recent_users, $list_id = LIST_ID ) {
 							'FNAME' => $first_name,
 							'LNAME' => $last_name,
 						],
+						'tags' => $user->member_type,
 					]
 				);
 			} catch ( Exception $e ) {
@@ -163,6 +318,7 @@ function update_mailchimp( $recent_users, $list_id = LIST_ID ) {
 				log_entry( 'Error adding ' . $user->user_email . ' to list.' );
 				log_entry( print_r( $response, true ) );
 			} else {
+				$added_users[] = $user;
 				log_entry( 'Added ' . $user->user_email . ' to list.' );
 			}
 		}
@@ -170,6 +326,8 @@ function update_mailchimp( $recent_users, $list_id = LIST_ID ) {
 			unset( $current_members[ $user->user_email ] );
 		}
 	}
+
+	$removed_users = [];
 
 	// Remove users who are no longer active.
 	foreach ( $current_members as $email => $member ) {
@@ -185,10 +343,16 @@ function update_mailchimp( $recent_users, $list_id = LIST_ID ) {
 				log_entry( 'Error removing ' . $email . ' from list.' );
 				log_entry( print_r( $response, true ) );
 			} else {
+				$removed_users[] = $user;
 				log_entry( 'Removed ' . $email . ' from list.' );
 			}
 		}
 	}
+
+	return [
+		'added' => $added_users,
+		'removed' => $removed_users,
+	];
 }
 
 /**
@@ -215,7 +379,7 @@ function mailchimp_connect() {
  * @return array Array of Mailchimp members.
  */
 function get_current_list_members( $mc, $list_id ) {
-	// Get current list members. MailChimp API only returns 1000 members at a time.
+	// MailChimp API only returns 1000 members at a time.
 	$current_members = [];
 	$offset = 0;
 	do {
