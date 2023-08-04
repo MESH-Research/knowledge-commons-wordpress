b96d97183c822b14cc675624548ed6ac2b097c3e
Mike Thicke <thickemi@msu.edu>
2023-08-04 13:56:21 -0400

    Reset and relink plugin & themes directories

--- .lando/build-wordpress.sh
+++ .lando/build-wordpress.sh
@@ -8,8 +8,20 @@ docker-php-ext-enable xdebug
 touch /tmp/xdebug.log
 chown www-data:www-data /tmp/xdebug.log
 
+echo "Resetting plugins & themes directories..."
+rm -rf /app/site/web/app/plugins/*
+rm -rf /app/site/web/app/themes/*
+rm -rf /app/site/web/app/mu-plugins/*
+
 echo "Installing Composer dependencies..."
 cd /app
 composer update
+
+echo "Relinking Commons plugins & themes..."
+ln -s /app/ancillary-plugins/*/ /app/site/web/app/plugins/
+ln -s /app/core-plugins/*/ /app/site/web/app/plugins/
+ln -s /app/forked-plugins/*/ /app/site/web/app/plugins/
+ln -s /app/mu-plugins/*/ /app/site/web/app/mu-plugins/
+ln -s /app/themes/*/ /app/site/web/app/themes/
 
 echo "Finished running WordPress build script."
