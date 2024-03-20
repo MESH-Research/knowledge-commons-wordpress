e1d509121debaa4d007f79b75bd5550cd501bda1
Mike Thicke <thickemi@msu.edu>
2024-03-20 14:39:35 -0400

    staging efs cron jobs running but not all working

--- Dockerfile.php
+++ Dockerfile.php
@@ -6,15 +6,17 @@ COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
 
 ADD https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
 RUN chmod +x /usr/local/bin/install-php-extensions && \
-	install-php-extensions exif imagick zip memcached redis mysqli intl yaml pcov
+	install-php-extensions exif imagick zip memcached redis mysqli intl yaml
 
 ADD https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar /usr/local/bin/
 RUN chmod a+rx /usr/local/bin/wp-cli.phar && \
 	mv /usr/local/bin/wp-cli.phar /usr/local/bin/wp
 
+RUN apk update && apk add mysql-client 
+
 FROM base AS lando
 
-RUN apk update && apk add git mysql mysql-client py3-pip py-cryptography mandoc aws-cli linux-headers 
+RUN apk add git mysql py3-pip py-cryptography mandoc aws-cli linux-headers bash
 
 EXPOSE 9000
 
@@ -37,11 +39,20 @@ COPY composer.json /app/
 COPY composer.lock /app/
 COPY wp-cli.yml /app/
 
-RUN mkdir -p /app/site/web/app/uploads && \
-	chown -R www-data:www-data /app/site/web/app/uploads
+RUN chown -R www-data:www-data /app
+RUN chmod -R 755 /app
+
+# Linking uploads folders to EFS volume mounted at /media
+RUN mkdir -p /media && \
+	chown www-data:www-data /media && \
+	rm -rf /app/site/web/app/uploads && \
+	ln -s /media/uploads /app/site/web/app/uploads && \
+	rm -rf /app/site/web/app/blogs.dir && \
+	ln -s /media/blogs.dir /app/site/web/app/blogs.dir
 
 WORKDIR /app
 ENV COMPOSER_ALLOW_SUPERUSER=1
+# Should get rid of this script and just run the commands directly
 RUN chmod a+x /app/scripts/build-scripts/build-wordpress-cloud.sh && \
 	/app/scripts/build-scripts/build-wordpress-cloud.sh
 
