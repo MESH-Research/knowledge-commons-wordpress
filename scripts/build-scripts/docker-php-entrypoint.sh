3a96853315bcac1ce57b51fa6b7bc23063296d27
Mike Thicke <thickemi@msu.edu>
2024-05-22 12:26:17 -0400

    ensure simplesaml cert directory exists when building

--- scripts/build-scripts/docker-php-entrypoint.sh
+++ scripts/build-scripts/docker-php-entrypoint.sh
@@ -8,6 +8,7 @@ fi
 
 rm -f /app/config/all/simplesamlphp/cert/saml.pem
 rm -f /app/config/all/simplesamlphp/cert/saml.crt
+mkdir -p /app/config/all/simplesamlphp/cert
 echo "$SAML_PEM" > /app/config/all/simplesamlphp/cert/saml.pem
 echo "$SAML_CRT" > /app/config/all/simplesamlphp/cert/saml.crt
 
