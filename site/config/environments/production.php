73b1d2360cff6f784c1129d2b89f17f2fae9a187
Mike Thicke <thickemi@msu.edu>
2024-07-15 15:41:30 -0400

    Show user notice messages in production

--- site/config/environments/production.php
+++ site/config/environments/production.php
@@ -5,4 +5,4 @@
  */
 ini_set( 'log_errors', 'on' );
 ini_set( 'error_log', 'php://stderr' );
-ini_set( 'error_reporting', E_ERROR | E_WARNING | E_USER_ERROR | E_USER_WARNING );
+ini_set( 'error_reporting', E_ERROR | E_WARNING | E_USER_ERROR | E_USER_WARNING | E_USER_NOTICE );
