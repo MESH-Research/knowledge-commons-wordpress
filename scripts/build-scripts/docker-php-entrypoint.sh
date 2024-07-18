a4c025a974819de58250fc6ca079fbc0ef731076
Mike Thicke <thickemi@msu.edu>
2024-07-18 10:52:51 -0400

    Only activate redis dropin on production

--- scripts/build-scripts/docker-php-entrypoint.sh
+++ scripts/build-scripts/docker-php-entrypoint.sh
@@ -12,4 +12,8 @@ mkdir -p /app/config/all/simplesamlphp/cert
 echo "$SAML_PEM" > /app/config/all/simplesamlphp/cert/saml.pem
 echo "$SAML_CRT" > /app/config/all/simplesamlphp/cert/saml.crt
 
+if [ "$WP_ENV" = "production" ]; then
+        cp /app/site/web/app/plugins/redis-cache/includes/object-cache.php /app/site/web/app/object-cache.php
+fi
+
 exec "$@"
