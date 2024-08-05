6ba0f615182b5a4209b614ba266dec90a32c5c8c
Mike Thicke <thickemi@msu.edu>
2024-08-05 06:25:48 -0400

    Add dahd-tainacan build files

---
+++ themes/dahd-tainacan/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8b43cf06788869eb3b55131ce125cd7cc77f94fe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8b43cf06788869eb3b55131ce125cd7cc77f94fe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'fabianobn/slick' => array(
+            'pretty_version' => 'v1.8.2',
+            'version' => '1.8.2.0',
+            'reference' => '62bacb288031d1e370327e0e2aa3c8a582e1d040',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fabianobn/slick',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v4.5.2',
+            'version' => '4.5.2.0',
+            'reference' => '5f2480a90ab911babc53039835fe78c6fc12646d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v4.5.2',
+            ),
+        ),
+    ),
+);
