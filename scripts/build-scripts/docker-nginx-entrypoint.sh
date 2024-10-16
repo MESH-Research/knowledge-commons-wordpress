e88d4dfdb705dbfc5aa931125814754648c12341
Mike Thicke <thickemi@msu.edu>
2024-10-16 12:56:22 -0400

    Address nginx container exiting at startup.

--- scripts/build-scripts/docker-nginx-entrypoint.sh
+++ scripts/build-scripts/docker-nginx-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+echo "Starting custom entrypoint script"
+
 # Retrieve AWS secrets and export them as environment variables
 if [ -f /app/scripts/build-scripts/get-aws-secrets.sh ]; then
     echo "Retrieving AWS secrets..."
@@ -14,4 +16,7 @@ else
     echo "AWS secrets retrieval script not found. Skipping."
 fi
 
+echo "Executing original entrypoint with arguments: $@"
 exec /docker-entrypoint.sh "$@"
+
+echo "docker-entrypoint.sh has finished executing"
--- scripts/build-scripts/get-aws-secrets.sh
+++ scripts/build-scripts/get-aws-secrets.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if required environment variables are set
+ENV_FILE="/etc/environment"
+
 if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_REGION" ] || [ -z "$AWS_SECRETS_ARN" ]; then
     echo "Error: Required AWS environment variables are not set."
     exit 1
 fi
 
-# Retrieve the secret from AWS Secrets Manager
 secret=$(aws secretsmanager get-secret-value --secret-id "$AWS_SECRETS_ARN" --query SecretString --output text)
 
-# Check if the secret was retrieved successfully
 if [ $? -ne 0 ]; then
     echo "Error: Failed to retrieve secret from AWS Secrets Manager."
     exit 1
 fi
 
-# Parse the JSON and export each key/value pair
-while IFS='=' read -r key value; do
-    # Remove surrounding quotes if present
+echo "$secret" | jq -r 'to_entries[] | "\(.key)=\(.value)"' | while IFS='=' read -r key value; do
     value=$(echo "$value" | sed -e 's/^"//' -e 's/"$//')
-    # Export the key-value pair
     export "$key=$value"
-done < <(echo "$secret" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
+    echo "export $key='$value'" >> "$ENV_FILE"
+done
 
-# Optionally, you can print a success message
+chmod 600 "$ENV_FILE"
+
 echo "AWS secrets have been successfully retrieved and exported to the environment."
+echo "Environment variables have been written to $ENV_FILE"
+
+if ! grep -q "source $ENV_FILE" /etc/profile; then
+    echo "source $ENV_FILE" >> /etc/profile
+fi
