16a795c0edb1a7d7fcd5d101baad617435b36cb6
Mike Thicke <mikethicke@gmail.com>
2024-11-03 13:30:47 -0500

    Add 404 page to boss-child.

---
+++ themes/boss-child/404.php
@@ -0,0 +1,18 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="error-404">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for could not be found. Please check the URL or 
+        <a href="<?php echo home_url(); ?>" class="button">return to Knowledge Commomns</a>
+    </p>
+</div>
+
+<?php
+
+get_footer();
+
+?>
