4dd87a4dde323073000c394f125073413aa5492c
Mike Thicke <mikethicke@gmail.com>
2024-11-12 14:21:00 -0500

    Add plugins and themes directories to site config

--- site/config/application.php
+++ site/config/application.php
@@ -107,6 +107,8 @@ Config::define('WP_SITEURL', env('WP_SITEURL'));
  * Custom Content Directory
  */
 Config::define('CONTENT_DIR', '/app');
+Config::define('WP_PLUGIN_DIR', '/app/site/web/app/plugins');
+Config::define('WP_THEME_DIR', '/app/site/web/app/themes');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
 Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
