cc948043dbc85fbe61bbd774a5f821f5ee0ba03e
Mike Thicke <thickemi@msu.edu>
2025-01-21 13:49:59 -0500

    Filter for Poseidon fonts directory

--- plugins/humanities-commons/plugin-hooks.php
+++ plugins/humanities-commons/plugin-hooks.php
@@ -17,3 +17,16 @@ function hc_suppress_simplemag_alert() {
 	remove_action( 'admin_notices', 'simplemag_deactivated_admin_notice', 999 );
 }
 add_action( 'admin_notices', 'hc_suppress_simplemag_alert', 10, 0 );
+
+/**
+ * Changes the base directory for Poseidon fonts to the EFS mount point.
+ * 
+ * This attempts to get around a bug where the Poseidon theme tries to create
+ * a fonts directory, but the FS_CHMOD_DIR constant has not been set, causing
+ * a hard crash. For this to work, there must be an existing 'fonts' directory
+ * as a subdirectory to what is returned by this filter.
+ */
+function hc_poseidon_font_base_directory() {
+	return '/media/uploads/poseidon/';
+}
+add_filter( 'wptt_get_local_fonts_base_path' );
