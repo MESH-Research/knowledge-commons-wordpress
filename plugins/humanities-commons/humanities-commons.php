c28447ee65e8bf5a309e400da5a34ece4b519084
Dimitris Tzouris <tzouris@msu.edu>
2025-10-22 20:11:30 +0300

    Modify spammer check to work only on weekends
    Update user vetting to work only on weekends.

--- plugins/humanities-commons/humanities-commons.php
+++ plugins/humanities-commons/humanities-commons.php
@@ -1661,7 +1661,7 @@ class Humanities_Commons {
 	 * @return boolean
 	 */
 	public static function hcommons_vet_user() {
-		return true; // disable spammer check for now
+		//return true; // disable spammer check for now
 		$current_user = wp_get_current_user();
 		$member_types = (array)bp_get_member_type( $current_user->ID, false );
 		if ( empty( $member_types ) || ( 1 == count( $member_types ) && in_array( 'hc', $member_types ) ) ) {
@@ -1669,15 +1669,21 @@ class Humanities_Commons {
 		} else {
 			return true;
 		}
-
-		$timeDiff = time() - strtotime( $current_user->user_registered );
-
-		if ( $timeDiff < ( 60 * 60 * 24 ) ) {
-			return false;
+		
+		// Check if today is Saturday (6) or Sunday (0)
+    	$dayOfWeek = date('w');
+    	if ( $dayOfWeek == 0 || $dayOfWeek == 6 ) {
+        	$timeDiff = time() - strtotime( $current_user->user_registered );	
+			if ( $timeDiff < ( 60 * 60 * 24 ) ) {
+				return false;
+			} else {
+				return true;
+			}
 		} else {
-			return true;
-		}
-	}
+        // If it's not the weekend, return true (skip age check)
+        return true;
+    }
+}
 
 	/**
 	 * Unserializes the shib_email meta to return to the user as an array
